6042bd8115cd322825d2279840ed390d3368b326
AlfonsoVS23 <avilches.suazo@gmail.com>
2022-11-30 17:12:19 -0300

    subida de carpeta que no funca

---
+++ security1/src/main/java/com/security/security1/controller/AppController.java
@@ -0,0 +1,31 @@
+package com.security.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    
+
+    @GetMapping({"/","/login"})
+    public String index(){
+        // el retorno se tiene que llamar igual al html
+        return "index";
+    }
+
+    @GetMapping("/menu")
+    public String menu(){
+        return "menu";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
